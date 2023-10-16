82c141ce4ad43b8fc9494808a8e12fbff0fd2a11
mdshahid1009266 <148151453+mdshahid1009266@users.noreply.github.com>
2023-10-17 02:45:13 +0600

    Update api.js

--- frontend/src/project/api.js
+++ frontend/src/project/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = 'https://mern-blog-server-ad3rvse73-shahids-projects-29577fef.vercel.app/';
+const URL = 'https://mern-blog-server-nine.vercel.app/';
 export const signUp = async (userData) => {
     try {
         return await axios.post(`${URL}/signup`, userData)
